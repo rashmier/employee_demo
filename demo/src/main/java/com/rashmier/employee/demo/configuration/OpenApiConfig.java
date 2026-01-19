40b09c5ab49fc057ba0f2a05d7a87225ecb2bc61
Rashmier Ynawat <106635824+rashmier@users.noreply.github.com>
2026-01-19 11:31:10 +0800

    create bean configuration for my OpenApi

---
+++ demo/src/main/java/com/rashmier/employee/demo/configuration/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.rashmier.employee.demo.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI employeeOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee Management API")
+                        .description("RESTful API for managing employees")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Employee API Support")
+                                .email("rashmier@demo.example.com")));
+    }
+}
