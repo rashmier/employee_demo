4ac290d797b6b1a2f20cff26d76f717dce79f7be
Rashmier Ynawat <106635824+rashmier@users.noreply.github.com>
2026-01-19 11:35:32 +0800

    create custom api response class

---
+++ demo/src/main/java/com/rashmier/employee/demo/controller/api/CustomApiResponse.java
@@ -0,0 +1,17 @@
+package com.rashmier.employee.demo.controller.api;
+
+
+public class CustomApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T object;
+
+    public CustomApiResponse(HttpStatus status, String message, T object) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.object = object;
+    }
+}
